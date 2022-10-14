e685e926f2701fb65c0a7e973f72548a5de99aa5
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2022-10-14 16:17:28 +0530

    database connection 14/10/22

--- 4/index.php
+++ 4/index.php
@@ -5,13 +5,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 </head>
 <body>
-    <table>
+    <table class="table">
+    <thead class="table-light">
       <tr>
+      <th>ID</th>
         <th>Name</th>
-        <th>role</th>
-        <th>ID</th>
+        <th>role</th>     
         <th>joining_date</th>
     </tr>
 
@@ -31,10 +34,10 @@ if($conn){
     // echo "<pre>";
     // echo $row['emp_name'], ":" , $row['emp_role'];
     echo"<tr>";
-    echo"<th>".$row['emp_name']."</th>
-        <th>".$row['emp_role']."</th>
-        <th>".$row['id']."</th>
-        <th>".$row['joining_date']."</th>";
+    echo"<td>".$row['id']."</td>
+         <td>".$row['emp_name']."</td>
+         <td>".$row['emp_role']."</td>
+         <td>".$row['joining_date']."</td>";
         echo"</tr>";
    }
 }
