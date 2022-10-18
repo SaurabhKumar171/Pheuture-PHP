8c1225fe17f154bebc3cf4cd1412330f0fe9452f
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2022-10-18 13:31:44 +0530

    VAS Practice

---
+++ Json_VAS/optin_api.php
@@ -0,0 +1,22 @@
+
+
+<?php
+    // print_r($_REQUEST);
+   
+    // $result = file_get_contents("php://input");
+    $response = array(
+        "message"=>"string",
+        "inError"=>true,
+        "requestId"=>"string",
+        "code"=>"string",
+        "responseData"=>array(
+        "transactionId"=>"string",
+         "subscriptionResult"=>"string",
+         "subscriptionError"=>"string"
+
+        ));
+        $enc_response=json_encode($response);
+    
+    echo $enc_response;
+
+?>
